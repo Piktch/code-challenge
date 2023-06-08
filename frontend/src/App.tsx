75b742e66f089abcb657590f4f329bf4637da854
slavik8kovalenko <Slavik8kovalenko@gmail.com>
2023-06-09 01:05:27 +0200

    here I reach exactly 3 Hours.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TextField from '@mui/material/TextField';
@@ -8,6 +8,8 @@ import currencyExchange from "./types/currencyExchange";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import currencyArray from "./constants/currencyList"
+import Container from '@mui/material/Container';
+import { margin } from '@mui/system';
 
 function App() {
   const [fromCurrency, setFromCurrency] = useState("");
@@ -16,66 +18,71 @@ function App() {
   const [toValue, setToValue] = useState(0);
   return (
     <>
-    <Stack spacing={2} direction="row">
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={currencyArray}
-        sx={{ width: 300 }}
-        renderInput={(params) => {
-          return <TextField {...params} label="Currency" />
-        }}
-        onInputChange={(event, newInputValue) => {
-          setFromCurrency(newInputValue)
-        }}
-      />
-      <TextField 
-      id="fromValue" 
-      label="amount" 
-      variant="standard"      
-      type="number"
-      value={fromValue}
-      onChange={(e) => {
-        setFromValue(Number.parseFloat(e.target.value))
-      }}
-      />
-    </Stack>
-    <Stack spacing={2} direction="row">
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={currencyArray}
-        sx={{ width: 300 }}
-        renderInput={(params) => {
-          return <TextField {...params} label="Currency" />
-        }}
-        onInputChange={(event, newInputValue) => {
-          setToCurrency(newInputValue)
-        }}
-      />
-      <div>{toValue}</div>
-    </Stack>
-    <Button 
-    variant="contained"
-    onClick={()=>{
-      currencyService.runExchange({
-        from: fromCurrency,
-        fromValue: fromValue,
-        to: toCurrency,
-      })
-          .then((response: any) => {
-            console.log(response);
-            if(response.data.status == "success") {
-              setToValue(response.data.value)
-            }
-          })
-          .catch((e: Error) => {
-            console.log(e);
-          });
-    }}
-    >Convert</Button>
+      <Container sx={{mt: "2rem", mb: "1rem"}} maxWidth="sm">
+        <Stack spacing={2} direction="row">
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={currencyArray}
+            sx={{ width: 300 }}
+            renderInput={(params) => {
+              return <TextField {...params} label="Currency" />
+            }}
+            onInputChange={(event, newInputValue) => {
+              setFromCurrency(newInputValue)
+            }}
+          />
+          <TextField
+            id="fromValue"
+            label="amount"
+            variant="standard"
+            type="number"
+            value={fromValue}
+            onChange={(e) => {
+              setFromValue(Number.parseFloat(e.target.value))
+            }}
+          />
+        </Stack>        
+      </Container >
+      <Container sx={{mb: "1rem"}} maxWidth="sm">
+        <Stack spacing={2} direction="row">
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={currencyArray}
+            sx={{ width: 300 }}
+            renderInput={(params) => {
+              return <TextField {...params} label="Currency" />
+            }}
+            onInputChange={(event, newInputValue) => {
+              setToCurrency(newInputValue)
+            }}
+          />
+          <div>{toValue}</div>
+        </Stack>
+        </Container>
+        <Container maxWidth="sm">
+        <Button
+          variant="contained"
+          onClick={() => {
+            currencyService.runExchange({
+              from: fromCurrency,
+              fromValue: fromValue,
+              to: toCurrency,
+            })
+              .then((response: any) => {
+                console.log(response);
+                if (response.data.status == "success") {
+                  setToValue(response.data.value)
+                }
+              })
+              .catch((e: Error) => {
+                console.log(e);
+              });
+          }}
+        >Convert</Button>
+        </Container>
     </>
-    
   )
 }
 
